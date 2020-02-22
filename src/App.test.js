c7e5a12fa8485d468a3556facf5d5975d0ba89a2
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-02-22 00:42:50 -0500

    going to eject create react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+configure({ adapter: new Adapter() });
+
+it("renders without an error", () => {
+  const wrapper = shallow(<App />);
+
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+it("renders increment button", () => {});
+
+it("renders counter display", () => {});
+
+it("counter starts at 0", () => {});
+
+it("clicking the button increments the counter in the display", () => {});
